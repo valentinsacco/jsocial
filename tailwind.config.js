ba05f9df15bdb2574b0f70ff8eeca83dee02647e
valentinsacco <vasacco@hotmail.com.ar>
2022-03-08 21:36:06 -0300

    layout folder added to tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './layout/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                main: '#f0db4f',
+                text: '#323330'
+            }
+        }
+    },
+    plugins: []
 }
